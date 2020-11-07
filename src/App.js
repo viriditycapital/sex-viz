ac8637cd901eb3d5d56587f839751a3a3f258043
mikinty <youmichaelc@gmail.com>
2020-11-07 15:59:22 -0500

    plans

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+/**
+ * Plan:
+ * - Navbar probably, on the top for different data sets
+ *
+ * Current projects:
+ * - Virginity loss, number of sexual partners
+ * - Penis size
+ */
 class App extends React.Component {
   render () {
     return (
